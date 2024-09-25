bbfc5464da5cc926dc4cfc4a7d45047ddfee0ddd
Nolhan <nolhan.dumoulin@gmail.com>
2024-09-25 16:24:05 +0200

    ajout dépendances + test graphiques

--- testAPI.py
+++ testAPI.py
@@ -1,5 +1,15 @@
 import requests
+import pandas as pd
+import matplotlib.pyplot as plt
 
 params = {"km" : "6"}
 response = requests.get("https://impactco2.fr/api/v1/transport", params=params)
-print(response.text)
+#print(response.text)
+
+datas = pd.read_excel('Datas.ods')
+print(datas)
+print(datas["Distance"])
+
+fig, ax = plt.subplots()
+ax.plot(datas["Mode de transport"],datas["Distance"])
+plt.show()
