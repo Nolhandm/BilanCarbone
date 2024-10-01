9227ae55b1082207181db981e97efcf6c84b7b5a
Nolhan <nolhan.dumoulin@gmail.com>
2024-10-01 21:14:30 +0200

    modifs

--- testAPI.py
+++ testAPI.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 
 params = {"km" : "6"}
 response = requests.get("https://impactco2.fr/api/v1/transport", params=params)
-#print(response.text)
+#response = requests.get("https://data.ademe.fr/data-fair/api/v1/datasets/base-carboner/lines", params=params)
+print(response.text)
 
-datas = pd.read_excel('Datas.ods')
-print(datas)
-print(datas["Distance"])
+datas = pd.read_excel('Datas.ods',[0,1])
+print(datas[0])
+#print(datas["Distance"])
 
 fig, ax = plt.subplots()
-ax.plot(datas["Mode de transport"],datas["Distance"])
+#ax.plot(datas["Mode de transport"],datas["Distance"])
 plt.show()
